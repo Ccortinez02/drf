1337a910f1a19b5955a8f756a8496d0dc3f7ece3
Ccortinez02 <cristopher.cortinez@inacapmail.cl>
2024-12-06 04:11:00 -0300

    quinto commit

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'drfsimplecrud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'drfsimplecrud/static/'
+STATICFILES_DIRS = [BASE_DIR / "drfsimplecrud/static/"]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
